7128c77b61b16ee1e38e25ba9a8a714125afecf6
Ilona Skargovskaya <ilona.skars@gmail.com>
2023-08-15 13:07:00 +0300

    feat: add filter by date

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import EventsNewest from "../events/EventsNewest";
 import { Link } from "react-router-dom";
 import EventsByDate from "../events/EventsByDate";
@@ -7,12 +7,72 @@ import { CiLocationOn } from "react-icons/ci";
 import { GoCommentDiscussion } from "react-icons/go";
 import Header from "../../components/Header";
 import LastReviews from "../reviews/LastReviews";
+import axios from "axios";
 
 const Home = () => {
     const [selectedLocation, setSelectedLocation] = useState("");
+    const [selectedDate, setSelectedDate] = useState(""); // State for selected date
+    const [filteredEvents, setFilteredEvents] = useState([]); // State for filtered events
+
+     // Function to fetch filtered events by date
+    const fetchFilteredEvents = async () => {
+        try {
+            const response = await axios.get(`/api/events-by-date`, {
+                params: {
+                    date: selectedDate,
+                },
+            });
+            setFilteredEvents(response.data);
+        } catch (error) {
+            console.error('Error fetching filtered events:', error);
+        }
+    };
+
+    // Fetch filtered events whenever selectedDate changes
+    useEffect(() => {
+        if (selectedDate) {
+            fetchFilteredEvents();
+        }
+    }, [selectedDate]);
+
     return (
         <div className="home__page">
             <Header />
+
+{/* Add a form to choose the date */}
+<section className="home__filter-form">
+                <Container>
+                    <form onSubmit={(e) => { e.preventDefault(); fetchFilteredEvents(); }}>
+                        <label>
+                            Choose Date:
+                            <input
+                                type="date"
+                                value={selectedDate}
+                                onChange={(e) => setSelectedDate(e.target.value)}
+                                defaultValue={new Date().toISOString().split('T')[0]} // Set default to today
+                            />
+                        </label>
+                        <button type="submit">Filter</button>
+                    </form>
+                </Container>
+            </section>
+
+            {/* Display filtered events or a message if no events found */}
+            <section className="home__filtered-events">
+                <Container>
+                    <h3>Filtered Events</h3>
+                    {filteredEvents.length > 0 ? (
+                        filteredEvents.map((event) => (
+                            <div key={event.id}>
+                                <h4>{event.title}</h4>
+                                {/* Display other event information */}
+                            </div>
+                        ))
+                    ) : (
+                        <p>No events found for the selected date.</p>
+                    )}
+                </Container>
+            </section>
 
             <EventsByDate />
 
@@ -58,6 +118,7 @@ const Home = () => {
                     </div>
                 </Container>
             </section>
+
 
             <EventsNewest />
 
