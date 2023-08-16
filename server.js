bb54999b058358ca60e3ee2cd94a20e907dd5207
Ilona Skargovskaya <ilona.skars@gmail.com>
2023-08-16 19:15:22 +0300

    feat: try deploy

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import jwtAuth from "./routes/jwtAuth.js";
 import dashboard from "./routes/dashboard.js";
 import { s3Uploadv2 } from "./s3Service.js";
 import nodemailer from 'nodemailer';
+import path from 'path';
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -193,7 +194,7 @@ app.listen(process.env.PORT || 3002, () => {
 //------DEPLOY
 
 // Have Node serve the files for our built React app
-// app.use(express.static(path.resolve(__dirname, "./client/build")));
+//app.use(express.static(path.resolve(__dirname, "./client/build")));
 app.use(express.static(path.join(__dirname, "client/build")));
 
 // All other GET requests not handled before will return our React app
