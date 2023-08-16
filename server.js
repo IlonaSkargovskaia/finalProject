185f5d8ac26f570826027ad61e690a6fad324bd8
Ilona Skargovskaya <ilona.skars@gmail.com>
2023-08-16 19:04:15 +0300

    feat: try deploy2

--- server.js
+++ server.js
@@ -193,7 +193,7 @@ app.listen(process.env.PORT || 3002, () => {
 //------DEPLOY
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+// app.use(express.static(path.resolve(__dirname, "./client/build")));
 app.use(express.static(path.join(__dirname, "client/build")));
 
 // All other GET requests not handled before will return our React app
